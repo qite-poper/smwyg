4e4725fe153fdf319bf4562b7515af81d5b259c0
qite-poper <74030348+qite-poper@users.noreply.github.com>
2021-03-16 17:24:19 +0900

    Correct entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -ex
 
-echo 'asdf'
+rm -rf /app/.env*
+
+GETENV=`/usr/bin/php /docker/getenv.php`
+
+if [ "$GETENV" = "ok" ];then
+  cd /app/
+  npm run build
+else
+  echo "Error occured when trying to get env from heroku"
+fi
