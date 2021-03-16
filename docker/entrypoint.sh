836881b6f09c3586db1cc6d1c8b3652438a8dcd1
qite-poper <74030348+qite-poper@users.noreply.github.com>
2021-03-16 17:49:11 +0900

    Remove workdir

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,8 @@ cp /docker/getenv.php ./
 GETENV=`/usr/bin/php getenv.php`
 
 if [ "$GETENV" = "ok" ];then
+  rm -rf ./node_modules
+  npm install
   npm run build
 else
   echo "Error occured when trying to get env from heroku"
